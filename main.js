7bdc339e4f91b7a7f57501a1e8003ec47a831909
bekzodbaqoev15-maker <bekzodbaqoev15@gmail.com>
2026-09-15 21:02:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+let names = prompt("Isming nima ey esh**");
+let qattar = prompt("qatta yashersen")
+let kecha = prompt("kechasi 12 danima qilasan ")
+alert("Salom " + qattar + " yashuvchi " + names + " janoblari");
+alert("ni*** " + kecha + " shunaqa ishlar qilasammi hali" + " qara lekn")
